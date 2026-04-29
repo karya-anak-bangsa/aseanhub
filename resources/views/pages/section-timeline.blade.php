2a1b0a5156192a85a7ec7635e405fbcc3a098ead
karya-anak-bangsa <tkab.alamsyah@gmail.com>
2026-04-29 10:40:11 +0700

    Project ASEAN Hub

--- resources/views/pages/section-timeline.blade.php
+++ resources/views/pages/section-timeline.blade.php
@@ -13,12 +13,15 @@
 
 <section id="alumni" class="alumni section light-background">
     <div class="container"> {{-- data-aos="fade-up" --}}
+
         <div class="section-title">
             <h2>Timeline & Event</h2>
         </div>
+
         <div class="row gatherings-section">
             @foreach ($timelines as $item)
                 <div class="col-lg-4 col-md-6 mb-4">
+
                     <div class="gathering-card">
                         <div class="gathering-header align-items-center">
                             <div class="gathering-badge">
@@ -41,8 +44,14 @@
                             @endif
                         </div>
                     </div>
+
                 </div>
+                {{-- col --}}
             @endforeach
         </div>
+        {{-- row --}}
+
     </div>
+    {{-- container --}}
 </section>
+{{-- section --}}
